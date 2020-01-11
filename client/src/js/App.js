b52e9f18e403d92d6e7a19ac60718424393d5224
Cindy <cindyxmiao@gmail.com>
2020-01-11 23:32:09 +0100

    login page

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import NavBar from './Components/_navbar.js';
+import LoginPage from './Components/LoginPage.js';
+
+import ReactDOM from 'react-dom';
 
 var angry = require('../images/angry.jpg');
 var confused = require('../images/confused.jpg');
@@ -18,9 +21,12 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: 'Click the button to load data!'
+      message: 'Click the button to load data!',
+      isLoginOpen: true,
+      isRegisterOpen: false
     }
   }
+
 
   fetchData = () => {
     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
@@ -35,11 +41,18 @@ class App extends Component {
     })
   }
 
+
+
   render() {
     return (
       <div className="App">
         <NavBar/>
-        <div className="thumbnail-container">
+        <h1> Hello World</h1>
+        <div className="box-container">
+          <LoginPage/>
+       </div>
+
+        {/* <div className="thumbnail-container">
           <img className="thumbnail" src={angry} />
           <img className="thumbnail" src={confused} />
           <img className="thumbnail" src={crying} />
@@ -50,7 +63,7 @@ class App extends Component {
           <img className="thumbnail" src={sleepy} />
           <img className="thumbnail" src={smile} />
           <img className="thumbnail" src={terrified} />
-        </div>
+        </div> */}
       </div>
     );
   }
