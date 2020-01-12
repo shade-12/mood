a14c79eca1a2c926081bad4d7f207a1e00eedfcb
Ken Johnson <dragonkenkj@gmail.com>
2020-01-11 21:59:06 -0800

    Working fight page

---
+++ client/src/js/Components/_homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+// function HomePage(props) {
+//     return (
+//       <Homepage>
+//         <Homepage.Brand href="#home"><b><h3>Yooooooooo</h3></b></Homepage.Brand>
+//       </Homepage>
+//     );
+// }
+
+//export default HomePage;
