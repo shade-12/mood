30dad3f0ad05959587fbd525619485e0f6e6f338
Eric Yan <37171109+eqfy@users.noreply.github.com>
2020-01-12 09:24:17 -0800

    Added connection to Music API

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import NavBar from './Components/_navbar.js';
+import MoodEnterPage from './Components/MoodEnterPage.js';
 
 var angry = require('../images/angry.jpg');
 var confused = require('../images/confused.jpg');
@@ -24,32 +25,45 @@ class App extends Component {
 
   fetchData = () => {
     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+      .then((response) => {
+        // handle success
+        console.log(response.data) // The entire response from the Rails API
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    })
+        console.log(response.data.message) // Just the message
+        this.setState({
+          message: response.data.message
+        });
+      })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    
+    fetch('/api/form-submit-url', {
+      method: 'POST',
+      body: data,
+    });
+  }
+
+  submitMood(event) {
+    let mood = event.target.value;
+    alert(mood);
+    //this.setState({demo: event.target.value});
+    // event.preventDefault();
+    // const data = new FormData(event.target);
+    // let mood = data.get('mood');
+    // alert(mood);
+    //let mood = document.getElementById("mood").value;
+    //document.getElementById("demo").innerHTML = "hi";
   }
 
   render() {
     return (
       <div className="App">
         <NavBar/>
-        <div className="thumbnail-container">
-          <img className="thumbnail" src={angry} />
-          <img className="thumbnail" src={confused} />
-          <img className="thumbnail" src={crying} />
-          <img className="thumbnail" src={depressed} />
-          <img className="thumbnail" src={happy} />
-          <img className="thumbnail" src={impatient} />
-          <img className="thumbnail" src={naughty} />
-          <img className="thumbnail" src={sleepy} />
-          <img className="thumbnail" src={smile} />
-          <img className="thumbnail" src={terrified} />
+        <div className="box-container">
+          <MoodEnterPage/>
         </div>
       </div>
     );
