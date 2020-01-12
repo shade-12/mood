cef1585a6aab4af3f808726c4d39b815bf8d25b8
Ken Johnson <dragonkenkj@gmail.com>
2020-01-12 05:10:03 -0800

    Added option to add friends/hobby but doesn't actually do anything

--- client/src/js/Components/MyMood.js
+++ client/src/js/Components/MyMood.js
@@ -4,6 +4,8 @@ import Carousel from 'react-bootstrap/Carousel'
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 import '../../styles/mymood.scss';
 
@@ -52,19 +54,7 @@ class MyMood extends React.Component{
     
     return (
       <div>
-        <div id = "dotsdiv">
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dots">
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1" >Create Monster </Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Add Friend</Dropdown.Item>
-            <Dropdown.Item href="#/action-3"> Add Hobbies</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-
+        <Menu></Menu>
         <section class="container">
           <div class="one">
             <Image src={userIcon} roundedCircle id = "userIcon"/>
@@ -77,88 +67,162 @@ class MyMood extends React.Component{
         </section>
         <br></br>
 
-<PopUp></PopUp>
+        <PopUp></PopUp>
 
-<Carousel>
-  <Carousel.Item class = "carouselItem">
-    <img src = {m1}/>
-    <Carousel.Caption>
-      <div class = "dino-text">
-      <h3>Dino-ster</h3>
-      </div>
-    </Carousel.Caption>
-  </Carousel.Item>
+        <Carousel>
+          <Carousel.Item class = "carouselItem">
+            <img src = {m1}/>
+            <Carousel.Caption>
+              <div class = "dino-text">
+              <h3>Dino-ster</h3>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-  <Carousel.Item class = "carouselItem">
-    <img src = {m2}/>
-    <Carousel.Caption>
-      <div class = "dino-text">
-      <h3>Fat-bean</h3>
-      </div>
-    </Carousel.Caption>
-  </Carousel.Item>
+          <Carousel.Item class = "carouselItem">
+            <img src = {m2}/>
+            <Carousel.Caption>
+              <div class = "dino-text">
+              <h3>Fat-bean</h3>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-  <Carousel.Item>
-    <img src = {m3}/>
-    <Carousel.Caption>
-      <div class = "dino-text">
-        <h3>Fluff-ball</h3>
-      </div>
-    </Carousel.Caption>
-  </Carousel.Item>
+          <Carousel.Item>
+            <img src = {m3}/>
+            <Carousel.Caption>
+              <div class = "dino-text">
+                <h3>Fluff-ball</h3>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-  <Carousel.Item>
-    <img src = {m4}/>
-    <Carousel.Caption>
-      <div class = "dino-text">
-        <h3>Mad-alien</h3>
-      </div>
-    </Carousel.Caption>
-  </Carousel.Item>
+          <Carousel.Item>
+            <img src = {m4}/>
+            <Carousel.Caption>
+              <div class = "dino-text">
+                <h3>Mad-alien</h3>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-  <Carousel.Item>
-    <img src = {m5}/>
-    <Carousel.Caption>
-      <div class = "dino-text">
-        <h3>Stitch</h3>
-      </div>
-    </Carousel.Caption>
-  </Carousel.Item>
+          <Carousel.Item>
+            <img src = {m5}/>
+            <Carousel.Caption>
+              <div class = "dino-text">
+                <h3>Stitch</h3>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-  <Carousel.Item>
-    <img src = {m6}/>
-    <Carousel.Caption>
-      <div class = "dino-text">
-        <h3>Big-bird</h3>
-      </div>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-
-
-          {/* <div id = "user-info">
-            <h2>Userr</h2>
-            <Image src={userIcon} roundedCircle id = "userIcon"/>
-          </div>
-          <p>HTML</p>
-          <div class="container">
-          <div class="skills html">90%</div>
-</div> */}
-
-          {/* <h4> Monsters Defeated</h4>
-        <div className="thumbnail-container">
-        <img className="thumbnail" src={m1} onClick={() => imageClick('angry')}/>
-        <img className="thumbnail" src={m2} onClick={() => imageClick('confused')}/>
-        <img className="thumbnail" src={m3} onClick={() => imageClick('crying')}/>
-        <img className="thumbnail" src={depressed} onClick={() => imageClick('depressed')}/>
-        <img className="thumbnail" src={happy} onClick={() => imageClick('happy')}/>
-        <img className="thumbnail" src={sleepy} onClick={() => imageClick('sleepy')} /> */}
-      
+          <Carousel.Item>
+            <img src = {m6}/>
+            <Carousel.Caption>
+              <div class = "dino-text">
+                <h3>Big-bird</h3>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
      
-      <h1>{this.state.current_mood}</h1>
+        <h1>{this.state.current_mood}</h1>
       </div>
     );
   }
+}
+
+function Menu() {
+
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
+
+  const handle1Close = () => setShow1(false);
+  const handle1Show = () => setShow1(true);
+  const handle2Close = () => setShow2(false);
+  const handle2Show = () => setShow2(true);
+  const handle3Close = () => setShow3(false);
+  const handle3Show = () => setShow3(true);
+  
+  return (
+    <div>
+      <div id = "dotsdiv">
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dots">
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1" onClick={handle1Show}>Create Monster</Dropdown.Item>
+            <Modal show={show1} onHide={handle1Close}>
+              <Modal.Header closeButton>
+                <Modal.Title>Add Monster</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    placeholder="Username"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                  />
+                </InputGroup>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handle1Close}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+
+            <Dropdown.Item href="#/action-2" onClick={handle2Show}>Add Friend</Dropdown.Item>
+            <Modal show={show2} onHide={handle2Close}>
+              <Modal.Header closeButton>
+                <Modal.Title>Add Friend</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    placeholder="Username"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                  />
+                </InputGroup>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handle2Close}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+
+            <Dropdown.Item href="#/action-3" onClick={handle3Show}>Add Hobbies</Dropdown.Item>
+            <Modal show={show3} onHide={handle3Close}>
+              <Modal.Header closeButton>
+                <Modal.Title>Add Hobby</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <InputGroup className="mb-3">
+                  <FormControl
+                    placeholder="Hobby"
+                  />
+                </InputGroup>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handle3Close}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  );
 }
 
 function PopUp() {
